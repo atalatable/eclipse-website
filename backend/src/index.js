de3c9a5fadf577781e9c3559771e5516f9b554c0
Atalata <kenny.lutz@laposte.net>
2023-07-24 20:40:02 +0200

    added all members

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const { members } = require('./data');
-const path = require('path')
+const path = require('path');
+const fs = require('fs')
+const cors = require('cors');
 
 const app = express();
 app.use(express.json())
 const port = process.env.port || 5000;
 
+// For developpment puroposes only
+
+app.use(cors({credentials: true, origin: ["http://localhost:4200"]}))
+
 // API
 
 app.get('/api/members', (req, res) => {
-   res.send(members);
+    res.send(
+        JSON.parse(
+            fs.readFileSync(
+            path.join(__dirname, "data/members.json"))).members.
+            sort((a,b) => ( 0.5 - Math.random() ))
+    );
+});
+
+app.get('/api/members/lineups', (req, res) => {
+    res.send(JSON.parse(fs.readFileSync(path.join(__dirname, "data/members.json"))).lineups);
+});
+
+app.get('/api/members/lineups/:lineup', (req, res) => {
+    const lu = req.params.lineup;
+    res.send(JSON.parse(fs.readFileSync(path.join(__dirname, "data/members.json"))).members.filter(member => member.lineup.toLowerCase() == lu));
+});
+
+app.get('/api/socials', (req, res) => {
+    res.send(JSON.parse(fs.readFileSync(path.join(__dirname, "data/socials.json"))));
 });
 
 // Angular
@@ -20,6 +43,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve('app/index.html'));
 });
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log("Express Server listening on port " + port)
 });
